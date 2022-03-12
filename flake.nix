96f0775b645cc79be343f29cb082f81f0eac3a32
hugosenari <hugosenari@gmail.com>
2022-03-12 02:09:24 -0300

    chore: re index woocommerce-api-nim

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A Nim wrapper for the WooCommerce REST API'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."woocommerce-api-nim-master".url = "path:./master";
+  inputs."woocommerce-api-nim-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."woocommerce-api-nim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A Nim wrapper for the WooCommerce REST API'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-woocommerce-api-nim-master.flake = false;
+  inputs.src-woocommerce-api-nim-master.owner = "mrhdias";
+  inputs.src-woocommerce-api-nim-master.ref   = "refs/heads/master";
+  inputs.src-woocommerce-api-nim-master.repo  = "woocommerce-api-nim";
+  inputs.src-woocommerce-api-nim-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-woocommerce-api-nim-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-woocommerce-api-nim-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
